080d22ae93bd62a10f0a34497368b57500d35229
shuanakperi <speri0915@gmail.com>
2025-04-25 21:34:30 -0400

    drawing

---
+++ itis3135/scripts/sketch.js
@@ -0,0 +1,26 @@
+
+let bottomImg, topImg;
+
+function preload() {
+  
+  bottomImg = loadImage('/assets/parrot-color.png');
+  topImg = loadImage('/assets/parrot-bw.png');
+}
+function setup() {
+  describe(
+    'Black-and-white photograph of a parrot. The cursor, when dragged across the canvas, adds color to the photograph.'
+  );
+
+  createCanvas(720, 400);
+
+  
+  noCursor();
+  cursor('/assets/brush.png', 20, -10);
+
+  
+  image(topImg, 0, 0);
+}
+function mouseDragged() {
+  
+  copy(bottomImg, mouseX, mouseY, 20, 20, mouseX, mouseY, 20, 20);
+}
